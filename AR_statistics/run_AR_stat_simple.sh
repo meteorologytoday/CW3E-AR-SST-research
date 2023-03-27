afd3a73bb7ad5bee2e8c38aa9dc444c80bdde1e6
t2hsu <t2hsu@aha.ucsd.edu>
2023-03-27 11:22:00 -0700

    Finish

--- AR_statistics/run_AR_stat_simple.sh
+++ AR_statistics/run_AR_stat_simple.sh
@@ -11,7 +11,7 @@ yrng_str="${beg_year}-${end_year}"
 
 #python3 mk_IVT_IWV_stat.py --input $input_dir --beg-year $beg_year --end-year $end_year
 
-python3 mk_AR_stat.py --input-dir $input_dir --beg-year $beg_year --end-year $end_year --ncpu $ncpu
+python3 mk_AR_stat.py --input-dir $input_dir --beg-year $beg_year --end-year $end_year --ncpu $ncpu --overwrite
 
 #python3 mk_AR_interannual_stat.py --input $input_dir --beg-year $beg_year --end-year $end_year
 #python3 mk_AR_EOF.py --input $input_dir/AR_interannual_statistics_${yrng_str}.nc --output-dir $input_dir
